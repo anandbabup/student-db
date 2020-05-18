eb889e59492eb32a2f07276f68a424d24686a88c
anandbabup <anand.net@gmail.com>
2020-05-18 16:35:20 +0530

    add couple of files

---
+++ handler.js
@@ -0,0 +1,7 @@
+const cb0 = function (req, res, next) {
+    console.log('CB0')
+    next()
+  }
+
+
+  module.exports = cb0;
--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require('express')
+const middleware = require('./handler')
+const app = express()
+const port = 3000
+
+
+  
+  var cb3 = function (req, res) {
+    res.send('Hello from C! with middleware')
+  }
+  
+  app.get('/', [middleware, cb3])
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
